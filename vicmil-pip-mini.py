41b2ae4ec93b5281cd996ab5dc6749335f7064dc
02vicmil <vicmil.millberg@gmail.com>
2025-02-28 18:21:25 +0100

    add vicmil-pip-mini

---
+++ vicmil-pip-mini.py
@@ -0,0 +1,6 @@
+# install latest version of vicmil-pip in this file
+import urllib
+with urllib.request.urlopen('https://raw.githubusercontent.com/vicmil-work/vicmil-pip/refs/heads/main/vicmil-pip.py') as f:
+    html = f.read().decode('utf-8')
+    with open(__file__, "w") as this_file:
+        this_file.write(html)
