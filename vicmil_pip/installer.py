71c816bfb19c8dec9bac310c01c9fa0df3cce568
02vicmil <vicmil.millberg@gmail.com>
2025-03-01 11:23:05 +0100

    rename vicmil_mkdocs to just mkdocs

--- vicmil_pip/installer.py
+++ vicmil_pip/installer.py
@@ -305,7 +305,7 @@ package_general = {
     "tiny-obj-loader": GoogleDriveZipPackage("https://drive.google.com/file/d/1PLCBebGr_kuzzxSbUnJgJN8O6Kn_fpL9/view?usp=drive_link"),
     "socket.io-client-cpp": GoogleDriveZipPackage("https://drive.google.com/file/d/1lH9CF9kTNqbS6BdUKrwcQJybUeWVlzjX/view?usp=drive_link", large=True),
     "util_cpp": InstallFromGitRepo("https://github.com/vicmil-work/vicmil-util/archive/refs/heads/master.zip", package_name="util_cpp"),
-    "vicmil_mkdocs": InstallFromGitRepo("https://github.com/vicmil-work/vicmil-util/archive/refs/heads/vicmil_mkdocs.zip", package_name="vicmil_mkdocs"),
+    "mkdocs": InstallFromGitRepo("https://github.com/vicmil-work/vicmil-util/archive/refs/heads/vicmil_mkdocs.zip", package_name="mkdocs"),
     "emsdk": EmsdkInstall() # Custom installer
 }
 
